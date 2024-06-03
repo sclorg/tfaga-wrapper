b653312f18739b190ac93ab028b9a2cace97d537
Petr Hracek <phracek@redhat.com>
2024-06-03 11:49:23 +0200

    Update composes to RHEL-8.10 and RHEL-9.4 as they reached GA (#23)
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- export_variables.sh
+++ export_variables.sh
@@ -82,19 +82,19 @@ case "$os_test" in
   "rhel8")
     tmt_plan="rhel8$tmt_plan_suffix"
     context="$context_prefix RHEL8$context_suffix"
-    compose="RHEL-8.8.0-Nightly"
+    compose="RHEL-8.10.0-Nightly"
     ;;
   "rhel9")
     tmt_plan="rhel9$tmt_plan_suffix"
     context="$context_prefix RHEL9$context_suffix"
-    compose="RHEL-9.2.0-Nightly"
+    compose="RHEL-9.4.0-Nightly"
     ;;
   "rhel9-unsubscribed")
     os_test="rhel9"
     dockerfile="Dockerfile.$os_test"
     tmt_plan="rhel9-unsubscribed-docker"
     context="$context_prefix RHEL9 - Unsubscribed host"
-    compose="RHEL-9.2.0-Nightly"
+    compose="RHEL-9.4.0-Nightly"
     ;;
   ""|*)
     echo "::error::os_test '$os_test' is not valid"
