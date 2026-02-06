97633f5aa2b4d968c8abe1aeaa63f5b9bcddf53d
Petr Hracek <phracek@redhat.com>
2026-02-06 11:20:09 +0100

    Use plans that starts with the tmt_plan.
    The plans are available here: 
    https://github.com/sclorg/sclorg-testing-farm/tree/main/plans

--- export_variables.sh
+++ export_variables.sh
@@ -82,17 +82,17 @@ fi
 dockerfile=Dockerfile."$os_test"
 case "$os_test" in
   "c9s")
-    tmt_plan="c9s"
+    tmt_plan="^c9s"
     context="$context_prefix CentOS Stream 9$context_suffix"
     compose="CentOS-Stream-9"
     ;;
   "c10s")
-    tmt_plan="c10s"
+    tmt_plan="^c10s"
     context="$context_prefix CentOS Stream 10$context_suffix"
     compose="CentOS-Stream-10"
     ;;
   "fedora")
-    tmt_plan="fedora"
+    tmt_plan="^fedora"
     context="Fedora$context_suffix"
     compose="Fedora-latest"
     ;;
